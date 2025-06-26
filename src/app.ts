47905fbcda14716150a2aee459e03dbdb4c0cb5f
srseen <seereen.2540@gmail.com>
2025-06-26 18:01:55 +0700

    add employee routes to the Express app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import employeeRoutes from "./routes/employeeRoutes";
 
 const app = express();
 
 app.use(express.json());
+app.use("/employees", employeeRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello from Express!");
