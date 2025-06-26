ace83e65584170a7aa8902f27dd08b87c058867b
srseen <seereen.2540@gmail.com>
2025-06-26 17:19:48 +0700

    add employee routes for CRUD operations

---
+++ src/routes/employeeRoutes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (req, res) => {});
+
+router.get("/:id", (req, res) => {});
+
+router.post("/", (req, res) => {});
+
+router.put("/:id", (req, res) => {});
+
+router.delete("/:id", (req, res) => {});
+
+export default router;
